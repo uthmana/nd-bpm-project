4b17994e2c12ff965e2c1cc82b94e3b46a0e8acf
Uthman AHMAD <uthmana.muhammad@gmail.com>
2023-12-16 20:54:57 +0300

    chore: add invoce ui

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,20 +5,13 @@ import React from 'react';
 // Icon Imports
 import {
   MdHome,
-  MdOutlineShoppingCart,
-  MdBarChart,
   MdPerson,
-  MdLock,
   MdOutlineLogout,
-  MdOutlineFiberManualRecord,
-  MdOutlineOnDeviceTraining,
   MdEmail,
   MdGroupWork,
   MdOutlineBusiness,
   MdOutlineGroups3,
-  MdBrokenImage,
   MdOutlineMultilineChart,
-  MdRestore,
   MdSettings,
   MdTaskAlt,
 } from 'react-icons/md';
@@ -29,31 +22,6 @@ const routes = [
     layout: '/admin',
     path: 'dashboard',
     icon: <MdHome className="h-6 w-6" />,
-  },
-  // {
-  //   name: 'NFT Marketplace',
-  //   layout: '/admin',
-  //   path: 'nft-marketplace',
-  //   icon: <MdOutlineShoppingCart className="h-6 w-6" />,
-  //   secondary: true,
-  // },
-  // {
-  //   name: 'Data Tables',
-  //   layout: '/admin',
-  //   path: 'data-tables',
-  //   icon: <MdBarChart className="h-6 w-6" />,
-  // },
-  // {
-  //   name: 'Sign In',
-  //   layout: '/auth',
-  //   path: 'sign-in',
-  //   icon: <MdLock className="h-6 w-6" />,
-  // },
-  {
-    name: 'Profile',
-    layout: '/admin',
-    path: 'profile',
-    icon: <MdPerson className="h-6 w-6" />,
   },
   {
     name: 'Stock',
@@ -68,9 +36,9 @@ const routes = [
     icon: <MdOutlineGroups3 className="h-6 w-6" />,
   },
   {
-    name: 'Product',
+    name: 'Entry',
     layout: '/admin',
-    path: 'product',
+    path: 'entry',
     icon: <MdOutlineBusiness className="h-6 w-6" />,
   },
   {
@@ -80,7 +48,7 @@ const routes = [
     icon: <MdGroupWork className="h-6 w-6" />,
   },
   {
-    name: 'Invoices',
+    name: 'Invoice',
     layout: '/admin',
     path: 'invoice',
     icon: <MdTaskAlt className="h-6 w-6" />,
