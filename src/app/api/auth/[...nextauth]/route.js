170f91d51d8e58a59a6cc604d09cca3f3abdf8b7
Abdul Majeed <26191324+issifuabdulmajeed@users.noreply.github.com>
2023-12-23 01:42:27 +0300

    email api done

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,3 +5,4 @@ import { authOptions } from '../../../lib/auth/options';
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,19 +1,21 @@
-import { EmailTemplate } from '../../../components/offer/email-template';
+// pages/api/send.js
 import { Resend } from 'resend';
+import { NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+export  async function POST(request) {
 
-    return Response.json(data);
+  try {
+    const formData = await request.json();
+    const data = await resend.emails.send({
+      from: 'ND <majeed@ndindustriesbmp.com>',
+      to: formData.email, 
+      subject: formData.subject,
+      html: `<p>${formData.message}</p>`, // it can be a react component
+    });
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error });
+    return NextResponse.json({ error });
   }
 }
