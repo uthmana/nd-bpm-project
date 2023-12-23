779ff1c0d3ab626637ef6b42eef928d0f5956d2b
Uthman AHMAD <uthmana.muhammad@gmail.com>
2023-12-24 00:59:48 +0300

    fix: git ignore

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,9 +1,17 @@
 // pages/api/send.js
-
+import { PrismaClient } from '@prisma/client';
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
+const prisma = new PrismaClient();
+
 const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function GET(request: Request) {
+//   const allUsers = await prisma.user.findMany();
+//   // console.log(allUsers)
+//   return NextResponse.json({ allUsers });
+// }
 
 export async function POST(request: Request) {
   //TODO: check send type
