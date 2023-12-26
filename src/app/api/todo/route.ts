e7ea7b3ac33747a2afcae951d1831bdad2be2261
Uthman AHMAD <uthmana.muhammad@gmail.com>
2023-12-27 01:37:56 +0300

    Update route.ts

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -4,9 +4,9 @@ import prisma from '../../lib/db1';
 export async function POST(req: Request) {
   const { title } = await req.json();
 
-  await prisma.todo.create({
+/*  await prisma.todo.create({
     data: { title, complete: false },
-  });
+  });*/
 
   return NextResponse.json({ message: 'Created Todo' }, { status: 200 });
 }
