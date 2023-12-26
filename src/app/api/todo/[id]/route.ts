99e3397a82c2e80ce2eebfc569ad7e8af776e36c
Uthman AHMAD <uthmana.muhammad@gmail.com>
2023-12-27 01:39:30 +0300

    Update route.ts

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -7,31 +7,16 @@ export async function PATCH(
 ) {
   const { completed } = await req.json();
 
-  await prisma.todo.update({
+ /* await prisma.todo.update({
     where: {
       id: id,
     },
     data: {
       complete: completed,
     },
-  });
+  });*/
   return NextResponse.json({ message: 'Updated' }, { status: 200 });
 }
 
-export async function DELETE(req: Request) {
-  const { id } = await req.json();
 
-  await prisma.todo.delete({
-    where: {
-      id: id,
-    },
-  });
-  return NextResponse.json({ message: 'Deleted Item' }, { status: 200 });
-}
 
-export async function GET(req: Request, res: NextResponse) {
-  const data = await prisma.todo.findFirst({ where: { title: 'Test1' } });
-  if (!data) {
-    NextResponse.json(data);
-  }
-}
