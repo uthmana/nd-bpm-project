4a3b97f0e4de4aba9f5e75b11288efd551543f46
Shirazbello <shirazbello@gmail.com>
2023-12-31 03:51:39 +0300

    Added the customer seeding

---
+++ prisma/seedCustomer.ts
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import csvParser from 'csv-parser';
+import prisma from 'app/lib/db';
+import { $Enums, Prisma } from '@prisma/client';
+
+async function seedDatabase() {
+  const csvData = [];
+
+  fs.createReadStream('prisma/ND_Customers.csv', { encoding: 'utf-8' })
+    .pipe(csvParser({ separator: ',' }))
+    .on('data', (row) => {
+      csvData.push(row);
+    })
+    .on('end', async () => {
+      const headers = Object.keys(csvData[0]);
+
+      for (const row of csvData) {
+        const data1: Partial<Prisma.CustomerCreateInput> = {
+          companyName: '',
+        };
+
+        headers.forEach((header) => {
+          if (header.trim() === 'cardType') {
+            data1[header.trim()] =
+              $Enums.CardType[row[header.trim()] as keyof $Enums.CardType];
+          } else {
+            data1[header.trim()] = row[header.trim()];
+          }
+        });
+        console.log({ ...data1 });
+        await prisma.customer.create({
+          data: { ...data1, Offers: {} } as Prisma.CustomerCreateInput,
+        });
+      }
+
+      console.log('Data seeded successfully.');
+      await prisma.$disconnect();
+    });
+}
+
+seedDatabase();
