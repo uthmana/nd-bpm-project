5a32acf162a56523819d609cecfd88ef4acb6b23
Uthman AHMAD <uthmana.muhammad@gmail.com>
2024-01-01 21:13:48 +0300

    chore: add fault api

---
+++ src/app/api/entry/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from 'app/lib/db';
+import { Fault } from '@prisma/client';
+
+//All Faults
+export async function GET(req: NextRequest) {
+  try {
+    const fault = await prisma.fault.findMany();
+    if (!fault) {
+      throw new Error('No fault found');
+    }
+    return NextResponse.json(fault, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+// Create Fault
+export async function PUT(req: Request) {
+  try {
+    const result: Fault = await req.json();
+    const { traceabilityCode } = result;
+
+    if (!traceabilityCode) {
+      return NextResponse.json(
+        { message: 'You are missing a required data' },
+        { status: 401 },
+      );
+    }
+
+    const fault = await prisma.fault.create({
+      data: result,
+    });
+    if (!fault) {
+      throw new Error('No fault found');
+    }
+
+    return NextResponse.json({ fault }, { status: 200 });
+  } catch (error) {
+    if (error?.code === 'P2002') {
+      return NextResponse.json(
+        { error: 'Error ocured while creating fault' },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ error: 'Error ocured while creating fault' });
+  }
+}
