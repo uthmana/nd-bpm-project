17f91ede35404775b31c515a0577813b338df29a
Uthman AHMAD <uthmana.muhammad@gmail.com>
2024-01-06 23:11:59 +0300

    fix: fileViewer link

--- src/components/fileViewer/index.tsx
+++ src/components/fileViewer/index.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
+import { MdAttachFile } from 'react-icons/md';
 
 export default function FileViewer({ file }) {
-  const imageType = ['jpeg', 'jpg', 'png', 'webp', 'gif', 'svg'];
-  const videoType = ['mp3', 'mp4', 'mov', 'ogg', 'mpg', 'mpeg'];
-  if (!file) return;
-  const fileType = file.split('.')[-1];
-
-  if (imageType.includes(fileType)) {
-    return (
-      <a href={`/uploads/${file}`} target="_blank">
-        <img className="w-[40px]" src={`/uploads/${file}`} />
-      </a>
-    );
-  }
-
-  if (videoType.includes(fileType)) {
-    return (
-      <a href={`/uploads/${file}`} target="_blank">
-        <video width="320" height="240" controls>
-          <source src={`/uploads/${file}`} type={`video/${fileType}`} />
-        </video>
-      </a>
-    );
-  }
-
+  if (!file) return null;
   return (
-    <a href={`/uploads/${file}`} target="_blank" download>
-      <object data={`/uploads/${file}`} width="100%" height="100%"></object>
+    <a href={`/uploads/${file}`} target="_blank">
+      <MdAttachFile className="h-6 w-6" />
     </a>
   );
 }
