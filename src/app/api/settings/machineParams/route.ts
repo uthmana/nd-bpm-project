954ced6db09ddd51705fbb60866ea6fa0f8f56d1
Uthman AHMAD <uthmana.muhammad@gmail.com>
2024-02-11 16:13:41 +0300

    typescript fix

--- src/app/api/settings/machineParams/route.ts
+++ src/app/api/settings/machineParams/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from 'app/lib/db';
+import prisma from '../../../lib/db';
 import { checkUserRole } from 'utils/auth';
 import { MachineParams, Prisma } from '@prisma/client';
 
