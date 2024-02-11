b114f98ccd91989f1f6b39810950825fe25de260
Uthman AHMAD <uthmana.muhammad@gmail.com>
2024-02-11 03:01:03 +0300

    fix: PrismaClient singleton

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -3,14 +3,29 @@ import { PrismaClient } from '@prisma/client';
 const db_url = 'postgresql://postgres:mau1234@localhost:5433/postgres'; //Uthman
 //const db_url = 'postgresql://postgres:majeed123@localhost:5432/postgres'; //Majeed
 //const db_url="postgresql://postgres:159357@localhost:5432/bpms_Db?schema=public";  //Shiraz
-// {
-//   datasources: {
-//     db: { url: db_url },
-//   },
-// }
+const db_url_prod =
+  'postgres://default:x0aI3JEXFvzl@ep-royal-queen-a4scbpug-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15';
+const db_directUrl =
+  'postgres://default:x0aI3JEXFvzl@ep-royal-queen-a4scbpug.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  const db_dev = {
+    datasources: {
+      db: { url: db_url },
+    },
+  };
+
+  const db_prod = {
+    datasources: {
+      db: { url: db_url_prod, directUrl: db_directUrl },
+    },
+  };
+
+  if (process.env.NODE_ENV !== 'production') {
+    return new PrismaClient(db_dev);
+  }
+
+  return new PrismaClient(db_prod);
 };
 
 declare global {
