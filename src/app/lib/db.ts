2c687a9d475fa752a4a61d46034aa30549614242
Abdul Majeed <26191324+issifuabdulmajeed@users.noreply.github.com>
2024-10-15 09:39:56 +0300

    Update db.ts

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,40 +1,40 @@
-// import { PrismaClient } from '@prisma/client';
+ import { PrismaClient } from '@prisma/client';
 
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
+ const prismaClientSingleton = () => {
+   return new PrismaClient();
+ };
 
-// declare global {
-//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
+ declare global {
+   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+ }
 
-// const prisma = globalThis.prisma ?? prismaClientSingleton();
+ const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-// export default prisma;
+ export default prisma;
 
-// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+ if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
 
 //**************************CONNECTION FOR DEVELOPMENT ****************************************************** */
-import { PrismaClient } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client';
 //const db_url = 'postgresql://postgres:mau1234@localhost:5433/postgres'; //Uthman
-const db_url = 'postgresql://postgres:majeed123@localhost:5432/postgres'; //Majeed
+//const db_url = 'postgresql://postgres:majeed123@localhost:5432/postgres'; //Majeed
 
-const prismaClientSingleton = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return new PrismaClient();
-  }
-  return new PrismaClient({
-    datasources: {
-      db: { url: db_url },
-    },
-  });
-};
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+//const prismaClientSingleton = () => {
+//  if (process.env.NODE_ENV === 'production') {
+//    return new PrismaClient();
+//  }
+//  return new PrismaClient({
+//    datasources: {
+//      db: { url: db_url },
+//    },
+//  });
+//};
+//declare global {
+//  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+//}
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+//const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma;
+//export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+//if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
