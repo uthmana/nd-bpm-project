a68ecda07a074c95e6d8d0f9e132c1b7575e6517
Abdul Majeed <26191324+issifuabdulmajeed@users.noreply.github.com>
2024-12-28 14:28:40 +0300

    email edited

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 lg:px-8 xl:flex-row print:hidden">
       <p className="mb-4 text-center text-sm font-medium text-gray-600 sm:!mb-0 md:text-lg">
         <span className="mb-4 text-center text-sm text-gray-600 sm:!mb-0 md:text-base">
-          ©{new Date().getFullYear()} ND Türkiye Proces. Tüm Hakkları Saklıdır.
+          ©{new Date().getFullYear()} ND Türkiye Proses. Tüm Hakları Saklıdır.
         </span>
       </p>
       <div>
